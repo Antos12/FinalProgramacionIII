8dbf4d826ce89a8afcb067cf15f26ca5aca5dbaf
Sofia <sofiabosio5@gmail.com>
2022-11-15 17:56:23 -0300

    Mostrar productos para el administrador

--- Ecommerce/Ecommerce/src/main/java/com/ecommerce/Ecommerce/service/ProductoService.java
+++ Ecommerce/Ecommerce/src/main/java/com/ecommerce/Ecommerce/service/ProductoService.java
@@ -1,6 +1,8 @@
 package com.ecommerce.Ecommerce.service;
 
 import com.ecommerce.Ecommerce.model.Producto;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductoService {
@@ -8,5 +10,6 @@ public interface ProductoService {
     public Optional<Producto> get(Integer id);
     public void update(Producto producto);
     public void delete(Integer id);
+    public List<Producto> findAll();
 
 }
